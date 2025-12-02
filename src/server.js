d6566f8160537b016b923a0d841591f3d3e21720
Logan Lugez <llugez@f6r4s11.clusters.42paris.fr>
2025-12-02 20:16:30 +0100

    ready to init sqlite

--- src/server.js
+++ src/server.js
@@ -1,14 +1,47 @@
 import fastify from "fastify";
+import fastifyView from "@fastify/view";
+import fastifyStatic from "@fastify/static";
+import { fileURLToPath } from "node:url";
+import { dirname, join } from "node:path"
+import ejs from 'ejs'
 
-const app = fastify()
+const app = fastify();
 
-async function start () {
-    try {
-        await app.listen({port: 3000})
-    } catch (err)
-    {
-        console.error(err)
-        process.exit(1)
-    }
+const rootDir = dirname(dirname(fileURLToPath(import.meta.url)))
+
+app.register(fastifyView, {
+	engine: {
+		ejs
+	}
+})
+
+app.register(fastifyStatic, {
+	root: join(rootDir, 'public')
+})
+
+const posts = [
+	{
+		title: "Mon premier article"
+	},
+	{
+		title: "Mon deuxieme article"
+	}
+]
+
+app.get('/', (req, res) => {
+	res.view('templates/index.ejs', {
+		posts
+	})
+})
+
+async function  start () {
+	try {
+		await app.listen({port: 3000})
+	}
+	catch (err)
+	{
+		console.error(err)
+		process.exit(-1)
+	}
 }
 start()
